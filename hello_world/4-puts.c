974004f2796e4f891e62fd4b679cf1fbf4da07e4
root <you@example.com>
2023-10-10 18:59:21 -0700

    this

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,24 +1,9 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program
- *
- * Description: This function prints the required message followed by a newline
- * character using the puts function and returns 0 to indicate successful execution.
- *
- * Return: 0 (Success)
- */
 int main(void)
 {
-    /* Using puts function to print the message without a comma and with a newline character */
     puts("Programming is like building a multilingual puzzle");
-
-    /* Using puts function to print a newline character */
     puts("");
-
-    /* Using puts function to print "0" with a newline character */
     puts("0");
-
-    /* Program ends with a return value of 0 */
     return (0);
 }
