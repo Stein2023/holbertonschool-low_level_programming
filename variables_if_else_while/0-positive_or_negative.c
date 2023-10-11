640630bbb2c503913054ab1f853661859565bf27
root <you@example.com>
2023-10-11 07:17:13 -0700

    this

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,8 +9,6 @@
  * Return: 0 (Success)
  */
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 int main(void)
 {
@@ -22,7 +20,7 @@ int n;
 	if (n > 0)
 	printf("%d is positive\n", n);
 	else if (n < 0)
-	printf("d is negative\n", n);
+	printf("%d is negative\n", n);
 	else
 	printf("%d is zero\n", n);
 	return (0);
