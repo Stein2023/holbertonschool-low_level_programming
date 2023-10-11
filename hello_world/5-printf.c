b868684c7cb6f44a9469a227cd3ce07004b0c39b
root <you@example.com>
2023-10-11 05:24:58 -0700

    this

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,14 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
- * main - entry point of the program 
+ * main - Entry point of the program
+ *
+ * Description: This function prints a message followed by a newline character
+ * using the puts function and returns 0 to indicate successful execution.
+ *
+ * Return: 0 (Success)
  */
-
-int main (void)
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
