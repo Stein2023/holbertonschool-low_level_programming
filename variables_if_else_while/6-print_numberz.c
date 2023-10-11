9ade222ea227fdd7d4af0ad403ced4d2451c43c0
root <you@example.com>
2023-10-11 10:55:40 -0700

    this

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0.
+ */
+	int main(void)
+	{
+		char num;
+
+		for (num = 0; num < 10; num++)
+			putchar(num + '0');
+
+
+
+	putchar('\n');
+	return (0);
+	}
