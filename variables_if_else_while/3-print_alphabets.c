cf05a64fa161d61ed60139ce50d705b83f64b6ee
root <you@example.com>
2023-10-11 10:08:54 -0700

    this

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+	int main(void)
+
+	{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+
+	}
+	char A = 'A';
+
+	while (A <= 'Z')
+	{
+	putchar(A);
+		a++;
+	}
+
+	putchar ('\n');
+	return (0);
+	}
