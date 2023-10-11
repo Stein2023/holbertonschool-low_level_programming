725670f2105d3e337bca0d0d10ef1845d49621e2
root <you@example.com>
2023-10-10 19:22:40 -0700

    this

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,9 +2,13 @@
 
 /**
  * main - Entry point of the program
+ *
+ * Description: This function prints a message followed by a newline character
+ * using the puts function and returns 0 to indicate successful execution.
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
-*/
-
 {
     puts("Programming is like building a multilingual puzzle\n");
     return (0);
