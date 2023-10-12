2607b11828f9f85e594f9f9536d8687ad363ae5d
root <you@example.com>
2023-10-12 10:28:39 -0700

    this

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - this
+ *
+ * @n: this
+ *
+ * Return: go
+ *
+ */
+	int print_sign(int n)
+	{
+
+	int n = n;
+
+	if (n > 0)
+	{
+	return (1);
+	putchar ('+');
+	}
+
+	if (n == 0)
+	{
+	return (0);
+	putchar (0);
+	}
+
+	if (n < 0)
+	{
+	return (-1);
+	putchar (-1);
+
+	}
