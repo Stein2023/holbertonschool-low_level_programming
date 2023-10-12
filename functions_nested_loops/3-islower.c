80ba5697bc1016ad374f4fc290e084d68f4b8508
root <you@example.com>
2023-10-12 08:11:57 -0700

    this

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - checks for a lowercase character
+ *
+ * @c: function
+ *
+ * Return: thing
+ */
+	int _islower(int c)
+
+	{
+
+	int n = 'c';
+
+	if (n == c)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+	}
