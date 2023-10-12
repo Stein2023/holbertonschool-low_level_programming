f91866a7253ebea8ca3702787504d2972d0a4dfa
root <you@example.com>
2023-10-12 06:54:41 -0700

    this

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - this is showing the alphabet
+ */
+
+
+void print_alphabet(void);
+
+	int main(void)
+	{
+		printf("abcdefghijklmnopqrstuvwxyz\n");
+	}
+
