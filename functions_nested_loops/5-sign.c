415c42dee9d07f78f2ae18ed2f4c45505041a2e5
root <you@example.com>
2023-10-12 10:38:23 -0700

    this

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -13,20 +13,20 @@
 
 	if (n > 0)
 	{
-	return (1);
 	putchar ('+');
+	return (1);
 	}
 
 	if (n == 0)
 	{
-	return (0);
 	putchar (0);
+	return (0);
 	}
 
-	if (n < 0)
+	else
 	{
-	return (-1);
 	putchar (-1);
+	return (-1);
 	}
 
 	}
