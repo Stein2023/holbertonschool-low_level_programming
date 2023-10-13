4ce05af156c952d907112a322be72aa829782123
root <you@example.com>
2023-10-12 20:35:07 -0700

    this

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,11 @@
+#iinclude <stdio.h>
+#include "main.h"
+/**
+  * print_to_98 - Prints all natural numbers from n to 98
+  * @n: The number to start 
+  *
+  * Return: Always 0.
+  */
+void print_to_98(int n)
+{
+	}
