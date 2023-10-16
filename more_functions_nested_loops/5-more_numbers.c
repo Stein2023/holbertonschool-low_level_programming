63a0976f6f3d39ec476f946aecded9159dccc00a
root <you@example.com>
2023-10-16 05:57:24 -0700

    this

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *
+ *more_numbers - this thing
  *
  */
 
@@ -11,17 +11,19 @@ int c;
 int a;
 for (a = 0; a < 10; a++)
 {
-	for (c = 48; c <= 57; c++)
+	for (c = 0; c <= 14; c++)
 
 {
-	_putchar(c);
-	
+
+	if ( c >= 10)
+
+	_putchar('1');
+
+	_putchar(c % 10 + 48);
+
+
+}
+        _putchar('\n');
 }
 
-
-	for (c = 0; c<= 4; c++)
-	_putchar(c + 48);
-
 }
-}
-
