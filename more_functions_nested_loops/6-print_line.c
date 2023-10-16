4ebb319c9c2faed7b99cc09f0e6e464879a90d22
root <you@example.com>
2023-10-16 06:30:54 -0700

    this

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - this
+ *
+ * @n: this
+ *
+ * Return: this
+ *
+ */
+
+void print_line(int n)
+{
+
+int i;
+
+for (i = 0; i < n; i++)
+{
+	_putchar('_');
+
+}
+
+	_putchar('\n');
+
+}
