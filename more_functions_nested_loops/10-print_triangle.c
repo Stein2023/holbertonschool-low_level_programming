f311b92fcc0455a7b0a215f00d9334fc84298673
root <you@example.com>
2023-10-17 04:03:30 -0700

    ~

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_triangle - triangle
+ * @size: size of triangle
+ * Return: void
+ *
+ */
+
+
+void print_triangle(int size)
+{
+	int row, hashes, spaces;
+
+	if (size <= 0)
+			{
+						_putchar('\n');
+						
