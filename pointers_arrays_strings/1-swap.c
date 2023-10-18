a985923d8a71cc3b4202c873317f509dfe38f769
root <you@example.com>
2023-10-18 07:25:17 -0700

    this

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - variables
+ * @a: the letter
+ * @b: the letter
+ * main - this
+ * Return: swaps values
+ */
+
+void swap_int(int *a, int *b)
+{
+*a = *b;
+*b = *a;
+}
