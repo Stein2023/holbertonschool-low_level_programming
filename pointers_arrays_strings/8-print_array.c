e4fedd47c7a0804ca6f1f8df55f4fafdcd4d6c14
root <you@example.com>
2023-10-19 09:24:25 -0700

    this

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -5,32 +5,21 @@
  * print_array - array
  * @a: letter
  * @n: letter
- * @i: letter
  */
 
 void print_array(int *a, int n) 
 {
 
-	for (a = 0; i < n; i++)
+int i;
 
+	for (i = 0; i < n; i++)
 	{
-
-		printf("%d", a);
-
-	if (i < n - 1) 
-		
-		{
-
-		printf(", ");
-
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
 	}
-		}
-
-	printf("\n");
-
-
-
-
+		printf("\n");
 
 
 }
