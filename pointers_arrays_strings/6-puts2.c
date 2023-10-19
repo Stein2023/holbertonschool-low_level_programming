4f5aa08600fe96c00dd02d0d56c58265d38cfc6f
root <you@example.com>
2023-10-19 06:12:35 -0700

    this

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -11,7 +11,7 @@
 
 void puts2(char *str)
 
-{	
+{
 
 int len = 0;
 
@@ -20,7 +20,7 @@ while (str[len] != '\0')
 	len++;
 }
 
-for (int i = 0; i < len; i +=2)
+for (int i = 0; i < len; i +=2 )
 {
 	printf("%c", str[i]);
 
