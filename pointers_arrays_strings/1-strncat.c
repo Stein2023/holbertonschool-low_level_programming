e96b5554dcaf4a8c417de39613397ba7d9d414e6
root <you@example.com>
2023-10-20 09:50:25 -0700

    this

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * @dest: a variable
+ * @src: a variable
+ * @n: a variable
+ *
+ * Return: return a function that concatenates two strings
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+char *temp = dest
+
+	while (*dest != '\0')
+	{
+		dest++;
+
+	}
+
+	while (*src != '\0' && n > 0)
+	{
+		*dest = *src;
+		dest++
+		src++
+		n--;
+
+	}
+
+	dest = '\0';
+
+	return (temp);
+}
