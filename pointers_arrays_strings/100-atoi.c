c2fd88db6f6ae3345248687b43772756f5041aab
root <you@example.com>
2023-10-19 17:33:29 -0700

    this

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,25 +8,15 @@
 
 int _atoi(char *s)
 {
-
-int i;
-i = 1;
-
+int i = 1;
 unsigned int num = 0;
-
-do
-
-{
-
+do {
 if (*s == '-')
 i *= -1;
 else if (*s >= '0' && *s <= '9')
 num = num * 10 + (*s - '0');
 else if (num > 0)
 break;
-}
-while (*s++)
-{
+} while (*s++);
 return (num *i);
-}
 }
