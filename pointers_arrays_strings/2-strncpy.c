2030af5e7848c1f5a70bd354c0482da3a16a11c5
root <you@example.com>
2023-10-22 14:57:29 -0700

    this

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,15 +11,21 @@
  * Return: Pointer to dest
  */
 
+
 char *_strncpy(char *dest, char *src, int n)
 {
 
+	int i;
 
-	for (n = 0; src[n] != '\0'; n++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[n] = src[n];
+	dest[i] = src[i];
 	}
-	dest[n] = '\0';
+
+for (; i < n; i++)
+{
+	dest[i] = '\0';
+	}
 
 	return (dest);
 }
