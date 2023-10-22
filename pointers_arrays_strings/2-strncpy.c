878a865b791b9666f420be2923e739cf14e53703
root <you@example.com>
2023-10-22 14:29:03 -0700

    this

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * *_strcpy - See description
+ * _strcpy - See description
  * @dest: char type string
  * @src: char type string
  * Description: Copy the string pointed to by pointer `src` to
