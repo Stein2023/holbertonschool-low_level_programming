7a71d9a477992387c53f0f718b7828b5187f3269
root <you@example.com>
2023-10-22 18:46:49 -0700

    this

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -30,12 +30,15 @@
  * Return: Pointer to the modified string (same as input string).
  */
 
-char *string_toupper(char *str) {
+char *string_toupper(char *str)
+{
 	int i = 0;
 
-	while (str[i] != '\0') {
+	while (str[i] != '\0')
+	{
 
-	if (str[i] >= 'a' && str[i] <= 'z') {
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
 
 	str[i] = str[i] - 'a' + 'A';
 	}
