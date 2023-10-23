14a4ec87ee8214a5a1ee5733f2fcaab41f99252b
root <you@example.com>
2023-10-22 18:50:36 -0700

    this

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -7,29 +7,6 @@
  * Return: Pointer to the modiffied string
  */
 
-/**
- * string_toupper - Converts all lowercase letters of a string to uppercase.
- * @str: Pointer to the input string.
- *
- * Return: Pointer to the modified string (same as input string).
- */
-
-
-/**
- * string_toupper - Converts all lowercase letters of a string to uppercase.
- * @str: Pointer to the input string.
- *
- * Return: Pointer to the modified string (same as input string).
- */
-
-
-/**
- * string_toupper - Converts all lowercase letters of a string to uppercase.
- * @str: Pointer to the input string.
- *
- * Return: Pointer to the modified string (same as input string).
- */
-
 char *string_toupper(char *str)
 {
 	int i = 0;
