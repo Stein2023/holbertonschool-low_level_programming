2aad70f2c53b974086d433745724a4453db4a704
root <you@example.com>
2023-10-22 19:08:03 -0700

    this

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,35 @@
 #include "main.h"
 
 /**
- * cap_string - the function
- * @entry - the variable
+ * cap_string - Write a function that capitalizes all words of a string.
  *
+ * @entry: This is the input string
  *
+ * Return: String capitalized
  */
-
 char *cap_string(char *entry)
 {
-	int index, count;
+	int conversion, index, count;
 
-	char chars[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\t', '\n', '\0'};
+	char chars[] = {' ', ',', ';', '.', '!',
+			 '?', '"', '(', ')', '{', '}',  '\t', '\n', '\0'};
+	conversion = 32;
 
 	for (index = 0; entry[index] != '\0'; index++)
 	{
-	if (entry[index] >= 'a' && entry[index] <= 'z')
-	{
-
-	entry[index] = entry[index] - 'a' + 'A';
+		if (entry[index] >= 'index' && entry[index] <= 'z')
+		{
+			entry[index] =  entry[index] - conversion;
+		}
+		conversion = 0;
+		for (count = 0; chars[count] != '\0'; count++)
+		{
+			if (chars[count] == entry[index])
+			{
+				conversion = 32;
+				break;
+			}
+		}
 	}
-
-	for (count = 0; chars[count] != '\0'; count++)
-	{
-	if (chars[count] == entry[index])
-	{
-	break;
-	}
-	}
-	}
-	return entry;
+	return (entry);
 }
-
