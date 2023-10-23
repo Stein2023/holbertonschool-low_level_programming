eb01ff6290aabd9a0ba9931b37f78a9dde1365e9
root <you@example.com>
2023-10-22 18:57:10 -0700

    this

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,28 @@
 #include "main.h"
 
-/**
- * cap_string - Write a function that capitalizes all words of a string.
- *
- * @entry: This is the input string
- *
- * Return: String capitalized
- */
 char *cap_string(char *entry)
 {
-	int conversion, index, count;
+    int index, count;
 
-	char chars[] = {' ', ',', ';', '.', '!',
-			 '?', '"', '(', ')', '{', '}',  '\t', '\n', '\0'};
-	conversion = 32;
+    char chars[] = {' ', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\t', '\n', '\0'};
 
-	for (index = 0; entry[index] != '\0'; index++)
-	{
-		if (entry[index] >= 'index' && entry[index] <= 'z')
-		{
-			entry[index] =  entry[index] - conversion;
-		}
-		conversion = 0;
-		for (count = 0; chars[count] != '\0'; count++)
-		{
-			if (chars[count] == entry[index])
-			{
-				conversion = 32;
-				break;
-			}
-		}
-	}
-	return (entry);
+    for (index = 0; entry[index] != '\0'; index++)
+    {
+        // Check if the current character is a lowercase letter
+        if (entry[index] >= 'a' && entry[index] <= 'z')
+        {
+            // Convert lowercase letter to uppercase
+            entry[index] = entry[index] - 'a' + 'A';
+        }
+        // Check if the character is one of the specified delimiter characters
+        for (count = 0; chars[count] != '\0'; count++)
+        {
+            if (chars[count] == entry[index])
+            {
+                break; // No need to continue checking if it's a delimiter
+            }
+        }
+    }
+    return entry;
 }
+
