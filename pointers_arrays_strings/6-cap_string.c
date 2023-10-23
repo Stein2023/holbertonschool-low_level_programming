f515dd2f2517ae91545eecc486a633a0de619032
root <you@example.com>
2023-10-22 19:01:24 -0700

    this

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,27 +2,26 @@
 
 char *cap_string(char *entry)
 {
-    int index, count;
+	int index, count;
 
-    char chars[] = {' ', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\t', '\n', '\0'};
+	char chars[] = {' ', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\t', '\n', '\0'};
 
-    for (index = 0; entry[index] != '\0'; index++)
-    {
-        // Check if the current character is a lowercase letter
-        if (entry[index] >= 'a' && entry[index] <= 'z')
-        {
-            // Convert lowercase letter to uppercase
-            entry[index] = entry[index] - 'a' + 'A';
-        }
-        // Check if the character is one of the specified delimiter characters
-        for (count = 0; chars[count] != '\0'; count++)
-        {
-            if (chars[count] == entry[index])
-            {
-                break; // No need to continue checking if it's a delimiter
-            }
-        }
-    }
-    return entry;
+	for (index = 0; entry[index] != '\0'; index++)
+	{
+	if (entry[index] >= 'a' && entry[index] <= 'z')
+	{
+
+	entry[index] = entry[index] - 'a' + 'A';
+	}
+
+	for (count = 0; chars[count] != '\0'; count++)
+	{
+	if (chars[count] == entry[index])
+	{
+	break;
+	}
+	}
+	}
+	return entry;
 }
 
