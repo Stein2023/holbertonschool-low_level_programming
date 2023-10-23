48df15c6273af741da1520a26d18687ef7e351e9
root <you@example.com>
2023-10-23 06:10:47 -0700

    this

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * leet - the function name
+ * @letters: the parameter
+ *
+ *
+ * Return: the value
+ */
+
+char *leet(char *letters)
+{
+
+char lookup[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+
+char rep[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (int i = 0; letters[i] != '\0'; i++)
+	{
+		for (int j = 0; j < 10; ++j)
+
+	{
+		if (letters[i] == lookup[j])
+
+
+	{
+		letters[i] = rep[j];
+
+		break;
+
+	}
+
+	}
+
+return (letters);
+}
+}
