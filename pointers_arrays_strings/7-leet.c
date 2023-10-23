7629160bdef3e110aec04c262cf58a340c6c80f9
root <you@example.com>
2023-10-23 06:14:42 -0700

    this

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -15,9 +15,11 @@ char lookup[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 
 char rep[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
-	for (int i = 0; letters[i] != '\0'; i++)
+int i;
+int j;
+	for (i = 0; letters[i] != '\0'; i++)
 	{
-		for (int j = 0; j < 10; ++j)
+		for (j = 0; j < 10; ++j)
 
 	{
 		if (letters[i] == lookup[j])
