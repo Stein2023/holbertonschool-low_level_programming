7c62be0e9bc4bf1a78d77e9fb9964fc387cbcc93
root <you@example.com>
2023-10-24 07:38:33 -0700

    this

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_chessboard - the function
+ * @a: the parameter
+ */
+
+
+
+void print_chessboard(char (*a)[8])
+{
+
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+
+	for (j = 0; j < 8; j++)
+	{
+		printf("&c ", a[i][j]);
+
+	}
+
+	printf("\n");
+
+	}
+
+
+
+}
