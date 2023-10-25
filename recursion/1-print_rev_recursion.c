62c8ef3aca495ae6dec664a20700b5db89e0d424
root <you@example.com>
2023-10-25 07:02:33 -0700

    this

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - the function
+ * @s: the parameter
+ */
+
+
+void _print_rev_recursion(char *s)
+{
+
+	char tmp;
+	int i, len, len1;
+
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
+{
+	len++;
+
+}
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+
+{
+	tmp = s[i];
+	s[i] = s[len1];
+	s[len1--] = tmp;
+
+
+}
+}
