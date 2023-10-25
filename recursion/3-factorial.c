c4b15565dbc510f4a0b25d9b371eabb0dfd37b95
root <you@example.com>
2023-10-25 09:59:37 -0700

    this

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -16,14 +16,15 @@ int factorial(int n)
 	{
 	return (1);
 	}
+	else if (n < 0) 
+	{
+		return (-1);
+	}
 
 	else
 	{
 	return (n * factorial(n - 1));
 
 	}
-	else if (n < 0)
-	{
-		return (-1);
-	}
+
 }
