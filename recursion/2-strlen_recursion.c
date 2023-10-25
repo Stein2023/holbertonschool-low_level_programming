29caa2f59667a584cf8ccd4d04a248438b0d8b8f
root <you@example.com>
2023-10-25 07:58:24 -0700

    this

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -12,17 +12,14 @@
 int _strlen_recursion(char *s)
 {
 
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		s++;
+		return (0);
 
 	}
-
 	else
 	{
-		printf("%s", s);
+		_strlen_recursion(s + 1);
 	}
-
-	return (0);
 
 }
