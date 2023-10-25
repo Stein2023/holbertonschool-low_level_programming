0ff2eef98fe1661f64311abfaadfc3edd6bd165f
root <you@example.com>
2023-10-25 06:05:36 -0700

    this

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - the function
+ * @s: the parameter
+ */
+
+void _puts_recursion(char *s)
+{
+
+
+puts("this line");
+
+("\n");
+
+}
