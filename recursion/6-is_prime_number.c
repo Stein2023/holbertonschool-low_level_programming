b92e289833375e8dff3030839c47c4165642ab34
root <you@example.com>
2023-10-25 18:38:16 -0700

    this

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -3,20 +3,22 @@
 
 /**
  * is_prime_number - the function
- * @n: the parameter
- * @divisor: the second parameter
+ * @prime - the parameter
+ *
  *
  * Return: return 1 if the integer is prime otherwise return 0
  */
 
-int is_prime_number(int n, int divisor)
+int is_prime_number(int prime)
 {
-	if (n <= 1)
+	if (prime <= 1)
 		return (0);
-	if (divisor * divisor > n)
+	if (prime == 2)
 		return (1);
-	if (n % divisor == 0)
+	if ((prime % 2 == 0) || (prime % 3 == 0) || (prime % 5 == 0))
 		return (0);
-	return (is_prime_number(n, divisor + 1));
+	else if (prime % 9 == 0 || prime % 7 == 0)
+		return (0);
+	else
+		return (1);
 }
-
