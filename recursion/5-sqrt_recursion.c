f6b069490454f86b40836108d3b531756b9eb589
root <you@example.com>
2023-10-25 17:54:24 -0700

    this

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - The function
+ * my_sqrt_recursion - The function
  * @a: the parameter
  * @b: the second paramater
  *
@@ -10,15 +10,25 @@
  * Return: The natural square root of a number
  */
 
-int _sqrt_recursion(int a, int b)
+int my_sqrt_recursion(int a, int b)
 {
-
 	if (a == (b * b))
 		return (b);
 	else if ((b * b) >= a)
 		return (-1);
 	else
-		return (_sqrt_recursion(a, b + 1));
+		return (my_sqrt_recursion(a, b + 1));
 }
 
-
+/**
+ * _sqrt_recursion - This is my first function
+ * @n: This is my value
+ *
+ * Return: This is my result of the function my_sqrt_recursion
+ */
+int _sqrt_recursion(int n)
+{
+	if (n <= 0)
+		return (-1);
+	return (my_sqrt_recursion(n, 0));
+}
