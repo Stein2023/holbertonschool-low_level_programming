4acbe7d9512add22d69527600539fc66d4949862
root <you@example.com>
2023-10-27 05:18:18 -0700

    this

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - the function
+ * @argc: the first parameter
+ * @argv: the second parameter
+ *
+ *
+ * Return: Program that multiplies two numbers
+ */
+
+
+int main(int argc, char *argv[])
+{
+
+int a, b, sum;
+
+sum = a * b;
+
+	if (a * b)
+	{
+		printf("%d\n", sum);
+	}
+
+	else
+	{
+		printf("Error");
+		putchar('\n');
+		return (1);
+	}
+
+
+
+}
