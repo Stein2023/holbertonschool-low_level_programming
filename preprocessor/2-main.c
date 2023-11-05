67d32888bda9c04d5ff84b943a0b57bd9c40fb83
root <you@example.com>
2023-11-05 07:28:50 -0800

    this

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - the name of the function
+ * @void - the parameter
+ *
+ *
+ * Return: the file that os being compiled
+ */
+
 int main(void)
 {
 printf("%s\n", __FILE__);
