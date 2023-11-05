3bce053c65d577f50371fb1858d6cc6afae11c3b
root <you@example.com>
2023-11-05 07:46:51 -0800

    this

---
+++ preprocessor/4-sum.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+ * description:  function-like macro SUM(x, y) give sum of variables
+ */
+
+#define SUM(x, y) ((x) + (y))
+#endif
+
