7ea27abd64d1a1ebe09e437f4e507c991b27fa5b
root <you@example.com>
2023-11-06 06:22:15 -0800

    this

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,13 +1,13 @@
-#ifndef DOG_H
-#define DOG_H
+#include <stdlib.h>
 #include "dog.h"
 /**
  * struct dog - dog data
  * @name: name of the dog
  * @age: age of the dog
  * @owner: name of the owner of the dog
- * @d: the letter
  * Description: Defines metadata about the dog and type of dog's struct
+ * init_dog - the function name
+ * @dog_t: the variable
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
@@ -20,4 +20,3 @@ typedef struct dog
 	char *owner;
 } dog_t;
 }
-#endif
