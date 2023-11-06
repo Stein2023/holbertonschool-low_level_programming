37746f25b0c8a9dbe9759079e011bb721987f89b
root <you@example.com>
2023-11-06 06:14:17 -0800

    this

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_STRUCT_H
-#define DOG_STRUCT_H
-
+#define DOG_STRUCT_H dog.h
 /**
  * struct dog - the dog
  * @name: the name
@@ -17,5 +16,5 @@ char *name;
 float age;
 char *owner;
 };
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
