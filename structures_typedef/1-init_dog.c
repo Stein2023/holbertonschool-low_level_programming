dff876d7987ead308ed9492a22031f1cbc403071
root <you@example.com>
2023-11-06 05:43:11 -0800

    this

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#ifndef STRUCT_DOG_H
+#define STRUCT_DOG_H
+
+/**
+ * init_dog - the function name
+ * struct dog - the struct name
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+ * @d: is a letter
+ *
+ * Return a variable of type struct dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct dog my_dog;
+}
+#endif
