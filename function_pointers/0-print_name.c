b27f19b413e2293c94d7f72e7844149daba0c06b
root <you@example.com>
2023-11-08 09:33:20 -0800

    this

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "function_pointers.h"
-#ifndef POINTERS_H
-#define POINTERS_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
 /**
  * print_name - Print a name for a fuction for pointer
  * @name: char string
