ef3c3ad090ce53ca748070a87b300484f38292d1
root <you@example.com>
2023-11-09 08:55:19 -0800

    this

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,38 +1,30 @@
-#include <stdio.h>
-#include <string.h>
+#include "3-calc.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * 
- * get_op_func - sum of a and b
- * @s: a parameter
+ * get_op_func - select the correct operation function asked by user
+ * @s: operator argument
+ * Return: function pointer corresponding to operator given
  */
-
-
 int (*get_op_func(char *s))(int, int)
 {
-
-op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
-int i;
-i = 0;
+	int i;
 
-while (i < 5)
-{
-	if (strcmp(s, ops[i].op) == 0)
-		return (ops[i].f;
-
-	i++;
-}
-
-	return (0);
-
-
-
+	i = 0;
+	while (i < 5)
+	{
+		if (ops[i].op[0] == s[0])
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
