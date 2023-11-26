7142813ee2cb208aa1629f77bbad80520f0488a9
Stein2023 <martinstein989@gmail.com>
2023-11-26 15:38:10 -0800

    node

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * dlistint_len - the name of the function
+ * @h: the pointer that takes in the elements
+ *
+ *
+ * Return: the number of elements in a linked list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+
+size_t counter;
+
+for (counter = 0; h; counter++)
+{
+	h = h->next;
+}	
+	return (counter);
+}
+
