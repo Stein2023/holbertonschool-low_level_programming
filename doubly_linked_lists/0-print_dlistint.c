888fbf767535dfc432d5bd65fc16218f6da33018
Stein2023 <martinstein989@gmail.com>
2023-11-26 15:04:36 -0800

    node

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -14,30 +14,21 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	
+
 const dlistint_t *current = h;
 size_t nodeCount = 0;
 
 	while (current != NULL)
 	{
-	printf("%d",current->n);
+	printf("%d\n", current->n);
 
-	if (current->next != NULL)
-	{
-	printf( " -> ");
-	}
-
-	else
-	{
-	printf ( "\n");
-	}
 
 	current = current->next;
 	nodeCount++;
 	}
 
 	return (nodeCount);
-	}
+}
 
 
 
