3b6eae053c0e7f50c363b73bf4935953250151ba
Stein2023 <martinstein989@gmail.com>
2023-11-27 18:10:21 -0800

    node

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -4,49 +4,45 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - the name of the function
+ * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: points to the list
  * @idx: the index for the counter
  * @n: the integers
  *
- *
  * Return: A new node at any given position
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+unsigned int i = 0;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t)), *tmp_node = *h;
 
-	 dlistint_t *temp;
-	 dlistint_t *new_node;
-	 unsigned int count;
-
-	temp = *h;
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+	if (new_node)
+	{
+		new_node->n = n;
+		if (idx == 0)
+		{
+			if (*h)
+			{
+				new_node->next = *h;
+				(*h)->prev = new_node;
+			}
+			*h = new_node;
+			return (new_node);
+		}
+		while (i < idx - 1 && tmp_node)
+		{
+			tmp_node = tmp_node->next;
+			i++;
+		}
+		if (tmp_node)
+		{
+			if (tmp_node->next)
+				tmp_node->next->prev = new_node;
+			new_node->next = tmp_node->next;
+			new_node->prev = tmp_node;
+			tmp_node->next = new_node;
+			return (new_node);
+		}
+	}
 	return (NULL);
-	new_node->prev = NULL;
-	new_node->next = NULL;
-	new_node->n = n;
-	if (idx == 0)
-	{
-	*h = new_node;
-	return (new_node);
-	}
-	else
-	{
-	count = 0;
-		while (temp != NULL && count != idx - 1)
-		{
-			temp = temp->next;
-			count++;
-		}
-		if (count != idx - 1)
-		{
-			return (NULL);
-		}
-		temp->next = new_node;
-		new_node->next = temp;
-		new_node->prev = temp;
-		return (new_node);
-	}
 }
