e4f7479a180d6016a1e0b9a8a9b0b4a9bca126f2
Stein2023 <martinstein989@gmail.com>
2023-11-29 05:51:06 -0800

    binary

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * clear_bit - the name of the function
+ * @n: The parameter
+ * @index: the index
+ *
+ *
+ * Return: 1 if it worked or -1 if an error occured
+ */
+
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+	return (-1);
+	}
+
+	*n &= ~(1UL << index);
+	return (1);
+}
