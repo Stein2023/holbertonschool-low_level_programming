7b62a10915497999c78086b41937536f9a535c37
Stein2023 <martinstein989@gmail.com>
2023-12-03 15:35:43 -0800

    hash

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -9,11 +9,12 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	if (ht == NULL)
-		return;
 
 	unsigned long int i;
 	hash_node_t *node, *temp;
+
+	if (ht == NULL)
+		return;
 
 	for (i = 0; i < ht->size; i++)
 	{
