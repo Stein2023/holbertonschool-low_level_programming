f561448e625f78065e97a716770e918101324482
Stein2023 <martinstein989@gmail.com>
2023-12-03 07:48:54 -0800

    hash

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -15,24 +15,25 @@ unsigned long int i;
 hash_table_t *ht;
 
 if (size == 0)
-	return (NULL);
+return (NULL);
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
-	return (NULL);
+ht = malloc(sizeof(hash_table_t));
+if (ht == NULL)
+return (NULL);
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
+ht->size = size;
+ht->array = malloc(sizeof(hash_node_t *) * size);
+
 if (ht->array == NULL)
-	{
-	free(ht);
-	return (NULL);
+{
+free(ht);
+return (NULL);
 }
-	for (i = 0; i < size; i++)
-	{
-	ht->array[i] = NULL;
-	}
+for (i = 0; i < size; i++)
+{
+ht->array[i] = NULL;
+}
 
-	return (ht);
+return (ht);
 }
 
